06513c1ee002dca09aaf5809ef190090ba236d57
Wouter De Borger <wouter.deborger@inmanta.com>
2019-05-23 10:13:53 +0200

    added test infra
    -port to native coroutines
    - pep8 compatible
    - added smoke test

--- server/src/inmantals/jsonrpc.py
+++ server/src/inmantals/jsonrpc.py
@@ -15,9 +15,8 @@
 
     Contact: code@inmanta.com
 """
-
+import asyncio
 from tornado.tcpserver import TCPServer
-from tornado import gen
 from tornado.iostream import StreamClosedError, BaseIOStream
 import logging
 import json
@@ -36,15 +35,14 @@ class ErrorCodes(Enum):
     MethodNotFound = -32601
     InvalidParams = -32602
     InternalError = -32603
-    serverErrorStart = -32099
-    serverErrorEnd = -32000
+    ServerErrorStart = -32099
+    ServerErrorEnd = -32000
     ServerNotInitialized = -32002
     UnknownErrorCode = -32001
     RequestCancelled = -32800
 
 
 class JsonRpcException(Exception):
-
     def __init__(self, message, id, **kwargs):
         super(JsonRpcException, self).__init__(message)
         self.id = id
@@ -55,23 +53,15 @@ class JsonRpcException(Exception):
         raise NotImplementedError()
 
     def to_dict(self):
-        error = {
-            "code": self.get_error_code().value,
-            "message": self.message
-        }
+        error = {"code": self.get_error_code().value, "message": self.message}
 
         if self.data is not None:
             error["data"] = self.data
 
-        return {
-            "jsonrpc": "2.0",
-            "id": self.id,
-            "error": error
-        }
+        return {"jsonrpc": "2.0", "id": self.id, "error": error}
 
 
 class ParseException(JsonRpcException):
-
     def __init__(self):
         super(ParseException, self).__init__("Could not parse JSON", None)
 
@@ -80,31 +70,26 @@ class ParseException(JsonRpcException):
 
 
 class InvalidRequestException(JsonRpcException):
-
     def get_error_code(self):
         return ErrorCodes.InvalidRequest
 
 
 class InternalErrorException(JsonRpcException):
-
     def get_error_code(self):
         return ErrorCodes.InternalError
 
 
 class MethodNotFoundException(JsonRpcException):
-
     def get_error_code(self):
         return ErrorCodes.MethodNotFound
 
 
 class InvalidParamsException(JsonRpcException):
-
     def get_error_code(self):
         return ErrorCodes.InvalidParams
 
 
 class JsonRpcServer(TCPServer):
-
     def __init__(self, delegate):
         """
             :param delegate: a class of which an instance is created for each connection, subclass of JsonRpcHandler
@@ -114,13 +99,12 @@ class JsonRpcServer(TCPServer):
             raise Exception("delegate must be a subclass of JsonRpcHandler")
         self.delegate = delegate
 
-    @gen.coroutine
-    def handle_stream(self, stream, address):
-        IOLoop.current().add_future(self.delegate(stream, stream, address).start(), lambda f: f.result())
+    async def handle_stream(self, stream, address):
+        f = asyncio.ensure_future(self.delegate(stream, stream, address).start())
+        f.add_done_callback(lambda fx: fx.result())
 
 
 class JsonRpcHandler(object):
-
     def __init__(self, instream: BaseIOStream, outstream: BaseIOStream, address):
         self.instream = instream
         self.outstream = outstream
@@ -133,23 +117,26 @@ class JsonRpcHandler(object):
         if field not in message:
             raise InvalidRequestException("header %s not found" % field, id)
         if value is not None and message[field] != value:
-            raise InvalidRequestException("expected header %s to be %s but was %s" % (field, value, message[field]), id)
+            raise InvalidRequestException(
+                "expected header %s to be %s but was %s"
+                % (field, value, message[field]),
+                id,
+            )
         return message[field]
 
-    @gen.coroutine
-    def decodeHeader(self):
+    async def decode_header(self):
         rn = "\r\n".encode(encoding="ascii")
         header = True
         contentlength = -1
         while header:
-            data = yield self.instream.read_until(rn)
+            data = await self.instream.read_until(rn)
             data = data.decode("ascii")
             if data == "\r\n":
                 header = False
             else:
                 parts = data[0:-2].split(": ", 2)
                 if len(parts) != 2:
-                    logger.error("Invalid header: " + header)
+                    logger.error("Invalid header: " + str(header))
                     return -1
                 header, value = parts
                 if header == "Content-Length":
@@ -159,54 +146,41 @@ class JsonRpcHandler(object):
                         logger.warn("unknown content type %s" % value)
         return contentlength
 
-    @gen.coroutine
-    def send(self, body: str):
-        with(yield self.writelock.acquire()):
+    async def send(self, body: str):
+        with (await self.writelock.acquire()):
             body = body.encode("utf-8")
             length = len(body)
             header = "Content-Length: %d\r\n\r\n" % length
-            header = header.encode(encoding='ascii')
-            yield self.outstream.write(header)
-            yield self.outstream.write(body)
+            header = header.encode(encoding="ascii")
+            await self.outstream.write(header)
+            await self.outstream.write(body)
 
-    @gen.coroutine
-    def return_error(self, excn: JsonRpcException):
+    async def return_error(self, excn: JsonRpcException):
         body = json.dumps(excn.to_dict())
-        yield self.send(body)
+        await self.send(body)
 
-    @gen.coroutine
-    def return_result(self, id, result):
-        body = {
-            "jsonrpc": "2.0",
-            "id": id,
-            "result": result
-        }
+    async def return_result(self, id, result):
+        body = {"jsonrpc": "2.0", "id": id, "result": result}
         body = json.dumps(body)
-        yield self.send(body)
+        await self.send(body)
 
-    @gen.coroutine
-    def send_notification(self, method, params):
-        body = {
-            "jsonrpc": "2.0",
-            "method": method,
-            "params": params
-        }
+    async def send_notification(self, method, params):
+        body = {"jsonrpc": "2.0", "method": method, "params": params}
         body = json.dumps(body)
-        yield self.send(body)
+        await self.send(body)
 
-    @gen.coroutine
-    def start(self):
+    async def start(self):
         self.running = True
 
         try:
             while self.running:
-                length = yield self.decodeHeader()
+                length = await self.decode_header()
                 if length == -1:
                     self.running = False
 
-                body = yield self.instream.read_bytes(length)
+                body = await self.instream.read_bytes(length)
                 body = body.decode("utf8")
-                future = self.decodeAndDispatch(body)
+                future = asyncio.ensure_future(self.decode_and_dispatch(body))
                 self.io_loop.add_future(future, lambda f: f.result())
         except StreamClosedError:
             pass
@@ -214,8 +188,7 @@ class JsonRpcHandler(object):
             self.instream.close()
             self.outstream.close()
 
-    @gen.coroutine
-    def decodeAndDispatch(self, body):
+    async def decode_and_dispatch(self, body):
         try:
             body = json.loads(body)
             if "id" in body:
@@ -223,18 +196,17 @@ class JsonRpcHandler(object):
             else:
                 id = None
             self.assert_field(body, "jsonrpc", "2.0", id=id)
-            yield self.dispatch(body, id)
-        except JSONDecodeError as e:
+            await self.dispatch(body, id)
+        except JSONDecodeError:
             logger.debug("Could not parse message", exc_info=True)
-            yield self.return_error(ParseException())
+            await self.return_error(ParseException())
         except JsonRpcException as e:
             logger.debug("Exception during processing", exc_info=True)
-            yield self.return_error(e)
-        except Exception as e:
+            await self.return_error(e)
+        except Exception:
             logger.error("exception during call handling", exc_info=True)
 
-    @gen.coroutine
-    def dispatch(self, body, id):
+    async def dispatch(self, body, id):
         if "method" in body:
             # is a request or notification call
             method = body["method"]
@@ -243,11 +215,11 @@ class JsonRpcHandler(object):
             else:
                 params = {}
             try:
-                result = yield self.dispatch_method(id, method, params)
+                result = await self.dispatch_method(id, method, params)
                 # if it has no id, it is a notification and we don't send result
                 if id is not None:
                     logger.debug("dispatching result %s", result)
-                    yield self.return_result(id, result)
+                    await self.return_result(id, result)
             except JsonRpcException as e:
                 logger.debug("exception occurred during method handling", exc_info=True)
                 # no exceptions on notifications
@@ -256,7 +228,9 @@ class JsonRpcHandler(object):
                     e.id = id
                     raise
             except Exception as e:
-                logger.debug("exception occurred during method handling ", exc_info=True)
+                logger.debug(
+                    "exception occurred during method handling ", exc_info=True
+                )
                 if id is not None:
                     # no exceptions on notifications
                     raise InternalErrorException(str(e), id)
@@ -267,6 +241,5 @@ class JsonRpcHandler(object):
         else:
             raise InvalidRequestException("no method, error or result field", id)
 
-    @gen.coroutine
-    def dispatch_method(self, id, method, params):
+    async def dispatch_method(self, id, method, params):
         raise MethodNotFoundException("method %s not found" % method, id)
