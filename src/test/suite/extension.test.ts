0c7200c372513e559388a1f7c069a0867f98f2ab
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2020-10-28 10:17:06 +0100

    wip

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -13,3 +13,51 @@ suite('Extension Test Suite', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
 });
+
+suite('Model compile tests', () => {
+
+	const modelBase: string = "" +
+		"entity Person:\n" +
+		"    string name\n" +
+		"    int age\n" +
+		"end\n\n" +
+		"Person.friends [0:] -- Person\n\n" +
+		"index Person(name)\n\n" +
+		"implement Person using std::none\n\n"
+
+	test('Valid model test', () => {
+		const model: string = modelBase +
+			"john = Person(name='John', age=20)\n" +
+			"mike = Person(name='Mike', age=21)\n" +
+			"jane = Person(name='Jane', age=19)\n" +
+			"lucy = Person(name='Lucy', age=23)\n\n" +
+			"john.friends += mike\n" +
+			"john.friends += jane\n\n" +
+			"mike.friends += jane\n" +
+			"mike.friends += john\n\n" +
+			"jane.friends += john\n" +
+			"jane.friends += mike\n"
+	});
+
+	test('Invalid model test', () => {
+		const model: string = modelBase +
+			"john = Person(name='John', age=20)\n" +
+			"mike = Person(name='Mike', age=21)\n" +
+			"jane = Person(name='Jane', age=19)\n" +
+			"lucy = Person(name='John', age=23)\n\n" +
+			"john.friends += mike\n" +
+			"john.friends += jane\n\n" +
+			"mike.friends += jane\n" +
+			"mike.friends += john\n\n" +
+			"jane.friends += john\n" +
+			"jane.friends += mike\n"
+	});
+});
+
+suite('CTRL + Click tests', () => {
+
+});
+
+suite('Venv installation tests', () => {
+
+});
