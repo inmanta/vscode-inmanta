23792ebcacf16f358b0e7da6bdcd5621686be3bd
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2020-10-29 11:11:00 +0100

    Checking that compilation occured successfully

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -2,6 +2,7 @@ import * as assert from 'assert';
 import { after, before } from 'mocha';
 import * as path from 'path';
 import * as fs from 'fs-extra';
+import * as readline from 'readline';
 
 import { Uri, window, commands, workspace, TextDocument } from 'vscode';
 
@@ -13,12 +14,17 @@ suite('Model compile tests', () => {
 	const libsPath: string = path.resolve(workspaceUri.fsPath, 'libs');
 	const envPath: string = path.resolve(workspaceUri.fsPath, '.env');
 
+	const logPath: string = '/tmp/vscode-inmanta.log'
+
 	before(() => {
 		console.log("Preparing test, cleaning out...");
 
 		// Ensuring project is clean
 		fs.removeSync(libsPath);
 		fs.removeSync(envPath);
+
+		// Removing log file
+		fs.removeSync(logPath)
 	});
 
 	test('Valid model test', async () => {
@@ -26,12 +32,33 @@ suite('Model compile tests', () => {
 		const modelUri: Uri = Uri.file(path.resolve(workspaceUri.fsPath, 'main.cf'));
 
 		const folder = await commands.executeCommand('vscode.openFolder', workspaceUri);
+
+		fs.ensureFileSync(logPath);
+
+		const readInterface = readline.createInterface({
+			input: fs.createReadStream(logPath)
+		});
+		
 		const doc: TextDocument = await workspace.openTextDocument(modelUri);
-		await doc.save();
 		const editor = await window.showTextDocument(doc);
 
 		// Waiting for the compilation to happen
-		await new Promise(resolve => setTimeout(resolve, 5000));
+		await new Promise(async resolve => {
+			const readLogInterval = setInterval(() => {
+				fs.readFile(logPath, 'utf-8', (err, data) => {
+					if (err) {
+						console.log(err);
+					} else if (data.includes('Compile succeeded')) {
+						clearInterval(readLogInterval);
+						resolve();
+					} else if (data.includes('Compile failed')) {
+						clearInterval(readLogInterval);
+						assert.strictEqual(false, true, "The compilation failed");
+						resolve();
+					}
+				});
+			}, 500);
+		});
 
 		const libsExists = fs.pathExistsSync(libsPath);
 		assert.strictEqual(libsExists, true, "The libs folder hasn't been created");
