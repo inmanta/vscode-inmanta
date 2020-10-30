4e0bac653bdb5c752fd4f94899bdb432add6bbfd
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2020-10-30 15:56:30 +0100

    Removed check on venv creation

--- src/test/suite/compile.test.ts
+++ src/test/suite/compile.test.ts
@@ -3,12 +3,12 @@ import { after, before, describe, it, beforeEach } from 'mocha';
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
-import { Uri, window, commands, workspace, TextDocument, TextEditor, Position, SnippetString } from 'vscode';
+import { Uri, window, commands, workspace, TextDocument, TextEditor, Position, SnippetString, extensions } from 'vscode';
 
 const logPath: string = '/tmp/vscode-inmanta.log';
 const workspaceUri: Uri = Uri.file(path.resolve(__dirname, '../../../src/test/workspace'));
 const libsPath: string = path.resolve(workspaceUri.fsPath, 'libs');
-const envPath: string = workspace.getConfiguration('inmanta').compilerVenv || path.resolve(workspaceUri.fsPath, '.env-ls-compiler');
+
 
 const modelUri: Uri = Uri.file(path.resolve(workspaceUri.fsPath, 'main.cf'));
 
@@ -50,7 +50,6 @@ describe('Compile checks', () => {
 		Promise.all([
 			fs.writeFile(logPath, ""),
 			fs.remove(libsPath),
-			fs.remove(envPath),
 			fs.remove(modelUri.fsPath),
 		]).then(async values => {
 			await commands.executeCommand('workbench.action.closeActiveEditor');
@@ -85,9 +84,6 @@ describe('Compile checks', () => {
 				const libsExists = fs.pathExistsSync(libsPath);
 				assert.strictEqual(libsExists, true, "The libs folder hasn't been created");
 
-				const envExists = fs.pathExistsSync(envPath);
-				assert.strictEqual(envExists, true, `The environment folder ("${envPath}") hasn't been created`);
-
 				resolve();
 			});
 		}).timeout(0);
@@ -97,7 +93,6 @@ describe('Compile checks', () => {
 		Promise.all([
 			fs.writeFile(logPath, ""),
 			fs.remove(libsPath),
-			fs.remove(envPath),
 			fs.remove(modelUri.fsPath),
 		]).then(values => {
 			done();
