50aff120798daed6b1fac79c628d476dc70dc9b6
Andras Kovari <andras.kovari@inmanta.com>
2020-10-30 11:02:00 +0100

    Correct venv path and add changelog entry

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # v 1.0.0 (?)
 Changes in this release:
 - Added error highlighting (#18)
+- Use separate virtual env for the compiler (#9)
 
 # v 0.0.1
 - Initial release
--- server/src/inmantals/server.py
+++ server/src/inmantals/server.py
@@ -67,7 +67,7 @@ class InmantaLSHandler(JsonRpcHandler):
         os.chdir(rootPath)
         init_options = kwargs.get("initializationOptions", None)
         if init_options:
-            self.compiler_venv_path = init_options.get("compilerVenv", ".env-ls-compiler")
+            self.compiler_venv_path = init_options.get("compilerVenv", os.path.join(self.rootPath, ".env-ls-compiler"))
 
         return {
             "capabilities": {
