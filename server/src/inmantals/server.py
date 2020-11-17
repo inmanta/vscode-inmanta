924dc3fb30c2cbdc9cdbf83ba3c2a300f46ae7e9
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2020-11-17 13:56:29 +0100

    Flake8

--- server/src/inmantals/server.py
+++ server/src/inmantals/server.py
@@ -64,18 +64,18 @@ class InmantaLSHandler(JsonRpcHandler):
         self.compiler_venv_path: Optional[str] = None
 
     async def initialize(
-        self, rootPath, rootUri, capabilities: Dict[str, object], **kwargs
+        self, root_path, root_uri, capabilities: Dict[str, object], **kwargs
     ):  # noqa: N803
         logger.debug("Init: " + json.dumps(kwargs))
 
-        if rootPath is None:
+        if root_path is None:
             raise Exception(
                 "A folder should be opened instead of a file in order to use the inmanta extension."
             )
 
-        self.rootPath = rootPath
-        self.rootUrl = rootUri
-        os.chdir(rootPath)
+        self.rootPath = root_path
+        self.rootUrl = root_uri
+        os.chdir(root_path)
         init_options = kwargs.get("initializationOptions", None)
         if init_options:
             self.compiler_venv_path = init_options.get(
@@ -267,10 +267,10 @@ class InmantaLSHandler(JsonRpcHandler):
         loc = list(range)[0].data
         return self.convert_location(loc)
 
-    async def textDocument_references(
-        self, textDocument, position, context
+    async def text_document_references(
+        self, text_document, position, context
     ):  # noqa: N802, N803
-        uri = textDocument["uri"]
+        uri = text_document["uri"]
 
         url = os.path.realpath(uri.replace("file://", ""))
 
