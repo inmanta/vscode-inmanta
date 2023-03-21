82ec949aa356031f13432dfd3e2ad969c0a3912a
Lejoly Florent <florent.lejoly@inmanta.com>
2023-03-21 11:01:21 +0100

    update

--- src/language_server.ts
+++ src/language_server.ts
@@ -368,6 +368,7 @@ export class LanguageServer {
 		if (canStart !== LanguageServerDiagnoseResult.ok){
 			return this.proposeSolution(canStart, this.diagnoseId);
 		}
+
 		if(start){
 			log("starting Language Server");
 		} else {
