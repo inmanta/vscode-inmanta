5de2dcfcf8a5d160be4a99ff2fdcfc402bae5a9a
hugo <hugo.thiolliere@inmanta.com>
2023-03-28 10:52:57 +0200

    [FIX]

--- server/tests/test_smoke.py
+++ server/tests/test_smoke.py
@@ -254,10 +254,10 @@ async def test_working_on_v1_modules(client, caplog):
     # This dummy module is used because it is only present in the dev artifacts and not in pypi index.
     assert not venv.are_installed(reqs)
 
-    options = {"repos": ("[" '{"url": "https://artifacts.internal.inmanta.com/inmanta/dev", "type": "package"},' "]")}
+    options = {"repos": [{"url": "https://artifacts.internal.inmanta.com/inmanta/dev", "type": "package"}, ]}
 
     path_uri = {"uri": f"file://{path_to_module}", "name": module_name}
-    ret = await client.call("initialize", workspaceFolders=[path_uri], capabilities={}, initializationOptions=options)
+    ret = await client.call("initialize", workspaceFolders=[path_uri], rootUri="file://{path_to_module}",capabilities={}, initializationOptions=options)
     result = await client.assert_one(ret)
     assert result == {
         "capabilities": {
@@ -313,10 +313,14 @@ async def test_working_on_v2_modules(client, caplog):
 
     assert "inmanta-module-module-v2" not in env.PythonWorkingSet.get_packages_in_working_set()
 
-    options = {"repos": ("[" '{"url": "https://pypi.org/simple", "type": "package"},' "]")}
+    options = {
+        "repos": [
+            {"url": "https://pypi.org/simple", "type": "package"},
+        ]
+    }
 
     path_uri = {"uri": f"file://{path_to_module}", "name": module_name}
-    ret = await client.call("initialize", workspaceFolders=[path_uri], capabilities={}, initializationOptions=options)
+    ret = await client.call("initialize", workspaceFolders=[path_uri], rootUri=f"file://{path_to_module}",  capabilities={}, initializationOptions=options)
     result = await client.assert_one(ret)
     assert result == {
         "capabilities": {
