239a3fd4bc22d87a13ec729109850f83646a43aa
FloLey <florent.lejoly@inmanta.com>
2023-05-05 16:46:20 +0200

    update

--- src/test/navigation/navigation.test.ts
+++ src/test/navigation/navigation.test.ts
@@ -40,7 +40,11 @@ describe('Language Server Code navigation', () => {
 			assert.strictEqual((pluginLocation as Location[]).length, 1);
 			assert.strictEqual(pluginLocation[0].uri.fsPath, path.resolve(libsPath, "testmodule", "plugins", "__init__.py"));
 			console.log("------------------------------------")
-			console.log(pluginLocation[0].range)
+			console.log(pluginLocation[0].range.start.line)
+			console.log(pluginLocation[0].range.start.character)
+			console.log(pluginLocation[0].range.end.line)
+			console.log(pluginLocation[0].range.end.character)
+
 			assert.deepStrictEqual(pluginLocation[0].range, new Range(new Position(4, 4), new Position(4, 22)), "Plugin location doesn't match");
 			resolve();
 		});
